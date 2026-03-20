9ba361c1f3452c78a338ee79360ea797900d8cfd
Giuliano <giuliano@rsic-edipo-805.homenet.telecomitalia.it>
2026-03-20 15:52:47 +0100

    remove my email

--- examples/threadLogger/threadLogger.cpp
+++ examples/threadLogger/threadLogger.cpp
@@ -1,7 +1,5 @@
 
 /*
- Copyright (C) Giuliano Catrambone (giuliano.catrambone@catrasoftware.it)
-
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either
